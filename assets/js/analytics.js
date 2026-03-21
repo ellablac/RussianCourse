00b70babfe5f0318b099c945bba5107f5ea2af71
Ella Blackledge <ellablac@gmail.com>
2026-03-20 23:59:32 -0500

    Added Google Analytics

---
+++ assets/js/analytics.js
@@ -0,0 +1,19 @@
+// Google Analytics loader for Sky4Tech projects
+
+(function () {
+  const GA_MEASUREMENT_ID = "G-XXXXXXXXXX"; // ← replace this
+
+  // Load gtag script dynamically
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+  document.head.appendChild(script);
+
+  // Init dataLayer
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ dataLayer.push(arguments); }
+  window.gtag = gtag;
+
+  gtag('js', new Date());
+  gtag('config', GA_MEASUREMENT_ID);
+})();
